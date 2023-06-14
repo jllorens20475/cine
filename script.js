5d0a352b1a3746b5fc866cc73469338a7c11d993
jllorens20475 <jllorens20475@iesjoanramis.org>
2023-06-14 17:26:40 +0200

    2d

--- script.js
+++ script.js
@@ -8,4 +8,35 @@ const peliculaSelect = document.getElementById('pelicula');
 
 let preuDelTicket = +peliculaSelect.value;
 
-console.log(preuDelTicket);
+function actualizasSeleccioSeients(){
+    const seientsSeleccionats = document.querySelectorAll('.fila .seient.seleccionat');
+    //console.log(seientsSeleccionats);
+
+    const seientsIndex = [...seientsSeleccionats].map(function(seient){
+        return [...seients].indexOf(seient);
+    });
+
+    const seientsIndexFletxa = [...seientsSeleccionats].map((seient) => [...seients].indexOf(seient));
+    
+    localStorage.setItem('seientsSeleccionats', JSON.stringify(seientsIndex));
+
+    const contadorSeients = seientsIndex.length;
+
+    contador.innerText = contadorSeients;
+    total.innerText = contadorSeients * preuDelTicket;
+}
+
+contenidor.addEventListener('click', (e) => {
+    //si es un seient i no esta ocupat
+    if (e.target.classList.contains('seient') &&  !e.target.classList.contains('ocupat')) {
+        //toggle per canviar de clase
+        e.target.classList.toggle('seleccionat');
+        actualizasSeleccioSeients();
+    }
+} )
+
+peliculaSelect.addEventListener('change', (e) => {
+    preuDelTicket = +e.target.value;   
+
+    actualizasSeleccioSeients();
+})
